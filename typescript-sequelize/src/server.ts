218e0ee8d67e09cccc566433dda99d23137d3f25
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2022-02-24 13:55:30 -0500

    express server with typescript

--- typescript-sequelize/src/server.ts
+++ typescript-sequelize/src/server.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+import express, {Request, Response} from 'express';
+
+const app = express(); 
+const port = 9000; 
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send("hello world"); 
+})
+
+app.listen(port, () => {console.log("who is listening?" + port)})
