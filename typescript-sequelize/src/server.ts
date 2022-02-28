d7764a817efb4ccdc06098a212c425f241201f13
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2022-02-27 21:26:22 -0500

    add update/:id

--- typescript-sequelize/src/server.ts
+++ typescript-sequelize/src/server.ts
@@ -40,7 +40,7 @@ app.post(
   }
 );
 
-// read with pagination 
+// read with pagination
 
 app.get(
   "/read",
@@ -49,7 +49,7 @@ app.get(
   async (req: Request, res: Response) => {
     const limit = req.query.limit as number | undefined;
     try {
-      const records = await TodoInterface.findAll({limit: limit });
+      const records = await TodoInterface.findAll({ limit: limit });
       // console.log("find the middleware?", req.body) //undefined--> need middleware
       // return res.send(createTodo);
       return res.json({ records, msg: "find records" }); //The res.json() function sends a JSON response.
@@ -63,18 +63,18 @@ app.get(
   }
 );
 
-// read with id  
+// read with id
 
 app.get(
   "/read/:id",
   TodoValidator.findParam(),
   Middleware.handleValidationError,
   async (req: Request, res: Response) => {
-    const id = req.params.id; 
-    console.log("id is: ", id); 
+    const id = req.params.id;
+    console.log("id is: ", id);
     try {
-      const record = await TodoInterface.findOne({where: {id: id } });
-      return res.json({ record, msg: "find this record" }); 
+      const record = await TodoInterface.findOne({ where: { id: id } });
+      return res.json({ record, msg: "find this record" });
     } catch (err) {
       return res.json({
         msg: " failed to find this record",
@@ -85,8 +85,34 @@ app.get(
   }
 );
 
-
 // update
+app.put(
+  "/update/:id",
+  TodoValidator.findParam(),
+  Middleware.handleValidationError,
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    console.log("id is: ", id);
+    try {
+      const record = await TodoInterface.findOne({
+        where: { id: id },
+      });
+      // update the completd
+      if (!record) {
+        return res.json({ msg: "did not find this id" });
+      }
+
+      await record.update({ completed: !record.getDataValue("completed") });
+      return res.json({ record, msg: "find this record" });
+    } catch (err) {
+      return res.json({
+        msg: " failed to find this record",
+        status: 500,
+        route: "/update/:id",
+      });
+    }
+  }
+);
 
 // delete
 
